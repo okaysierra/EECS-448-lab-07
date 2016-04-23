67e85b4e095b80e62facc9d269ce3f8cb1213350
okaysierra <scseacat@gmail.com>
2016-04-23 13:44:46 -0500

    Revert "Fix makefile, write more tests"
    This reverts commit f57727bce9aacf379e75ac4352f6dcaf6dea19d5.

--- main.cpp
+++ main.cpp
@@ -3,14 +3,8 @@
 int main()
 {
 	test test1;
-
-	test1.testIsEmpty();
-
-	test1.testSize();
-
-	test1.testSearch();
-
-	test1.testAddFront(); 
-
-	return 0;
-}
+	
+	test1.testIsEmpty(); 
+	
+	return 0; 
+} 
